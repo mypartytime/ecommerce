2a04a0b18d80924a8892738de6aeb450bd78b12b
mypartytime <140714609+mypartytime@users.noreply.github.com>
2023-10-11 15:36:20 +0700

    Loin and Logout Toastr

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -64,7 +64,13 @@ class UserController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/login');
+        $notification = array(
+            'message' => 'User Logout Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect('/login')->with($notification);
+        
     } // End Mehtod 
 
     public function UserUpdatePassword(Request $request){
